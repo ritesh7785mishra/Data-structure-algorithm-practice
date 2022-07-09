b805e943e5c3265a1758cd239618d2e06e935352
Ritesh Mishra <ritesh7785mishra@gmail.com>
2022-07-09 09:59:13 +0530

    First Recursion question

---
+++ src/Recursion/getStairsPath.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+import java.util.*;
+
+public class getStairsPath {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        ArrayList<String> paths = getStairPaths(n);
+        System.out.println(paths);
+    }
+
+
+    public static ArrayList<String> getStairPaths(int n) {
+
+        if(n==0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }else if(n<0){
+            ArrayList<String> bres = new ArrayList<>();
+            return bres;
+        }
+        ArrayList<String> paths1 = getStairPaths(n-1); //gives path for n-1
+        ArrayList<String> paths2 = getStairPaths(n-2); // gives all the paths for n-2
+        ArrayList<String> paths3  = getStairPaths(n-3); // gives all the paths for n-3
+
+        ArrayList<String> paths = new ArrayList<String>(); // Store an arraylist for all the paths
+
+        for (String path: paths1){
+            paths.add(1 + path);
+        }
+
+        for (String path: paths2){
+            paths.add(2 + path);
+        }
+        for (String path: paths3){
+            paths.add(3 + path);
+        }
+
+        return paths;
+    }
+}
