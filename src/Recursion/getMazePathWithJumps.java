23b1ea4ac8cc337379cce6a07569b495ce6400ba
Ritesh Mishra <ritesh7785mishra@gmail.com>
2022-07-11 08:43:44 +0530

    Create getMazePathWithJumps.java

---
+++ src/Recursion/getMazePathWithJumps.java
@@ -0,0 +1,19 @@
+package Recursion;
+import java.util.*;
+
+public class getMazePathWithJumps {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        return null;
+    }
+
+}
