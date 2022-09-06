cd09d20ab6ee6f369c541c8bc91ef2e3deeb7ae2
Ritesh Mishra <ritesh7785mishra@gmail.com>
2022-09-06 12:21:08 +0530

    stacks question

--- src/StacksAndQueues/nextGreaterElementToRight.java
+++ src/StacksAndQueues/nextGreaterElementToRight.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-public class nextGreaterElementToRight {
+public class nextGreaterElementToRight<nge> {
     public static void display(int[] a){
         StringBuilder sb = new StringBuilder();
 
@@ -31,27 +31,47 @@ public class nextGreaterElementToRight {
         //- A +
         // solve
 
-        int[] nge = new int[arr.length];
-        Stack<Integer> st = new Stack<>();
-        st.push(arr.length - 1);
-        nge[arr.length-1] = -1;
+//        int[] nge = new int[arr.length];
+//        Stack<Integer> st = new Stack<>();
+//        st.push(arr.length - 1);
+//        nge[arr.length-1] = -1;
+//
+//
+//
+//        for( int i= arr.length - 2; i>=0; i--){
+//            while(st.size() >0 && arr[i]>= st.peek()){
+//                st.pop();
+//            }
+//
+//            if(st.size()==0){
+//                nge[i] = -1;
+//            }else{
+//                nge[i] = st.peek();
+//            }
+//
+//            st.push(arr[i]);
 
+            int[] nge = new int[arr.length];
+            Stack<Integer> st = new Stack<>();
 
+            st.push(arr[arr.length-1]);
+            nge[arr.length - 1] = -1;
 
-        for( int i= arr.length - 2; i>=0; i--){
-            while(st.size() >0 && arr[i]>= st.peek()){
-                st.pop();
+            for(int i= arr.length - 2; i>=0; i--){
+                while(st.size() >0 && arr[i] >= st.peek()){
+                    st.pop();
+                }
+
+                if(st.size() == 0){
+                    nge[i] = -1;
+                }else{
+                    nge[i] = st.peek();
+                }
+
+                st.push(arr[i]);
             }
 
-            if(st.size()==0){
-                nge[i] = -1;
-            }else{
-                nge[i] = st.peek();
-            }
-
-            st.push(arr[i]);
+            return nge;
         }
 
-        return nge;
-    }
 }
---
+++ src/StacksAndQueues/ngetrAlternate.java
@@ -0,0 +1,56 @@
+package StacksAndQueues;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class ngetrAlternate {
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : a) {
+            sb.append(val + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] nge = solve(a);
+        display(nge);
+    }
+
+    public static int[] solve(int[] arr) {
+        // approach 2;
+        // we have to put index;
+
+        int[] nge = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+
+        st.push(0);
+
+        for(int i=1; i< arr.length; i++){
+           while(st.size()>0 && arr[i] > arr[st.peek()]){
+               int pos = st.peek();
+               nge[pos] = arr[i];
+               st.pop();
+           }
+           st.push(i);
+        }
+
+        while(st.size() > 0){
+            int pos = st.peek();
+            nge[pos] = -1;
+            st.pop();
+        }
+
+        return nge;
+    }
+}
